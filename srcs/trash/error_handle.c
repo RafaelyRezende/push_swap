3037526b2c5db1da1d0d089dd60c2533d9b1c7ad
Rafael Rezende <rafael.yanase@gmail.com>
2025-08-16 15:20:00 +0100

    Update: Added cleanup stack, node and env functions

--- srcs/trash/error_handle.c
+++ srcs/trash/error_handle.c
@@ -6,7 +6,7 @@
 /*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 09:03:47 by rluis-ya          #+#    #+#             */
-/*   Updated: 2025/08/15 17:27:16 by rluis-ya         ###   ########.fr       */
+/*   Updated: 2025/08/16 15:17:20 by rluis-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,54 @@ void	ft_exit_clean(char **split, long *arr_nums)
 		free(arr_nums);
 	if (split)
 		ft_exit_split(split);
+}
+
+void	ft_cleanup_node(t_node **node_addr)
+{
+	t_node	*node;
+
+	if (!node_addr || !*node_addr)
+		return ;
+	node = *node_addr;
+	if (node->value)
+		free(node->value);
+	free(node);
+	*node_addr = NULL;
+}
+
+void	ft_cleanup_stack(t_node **head)
+{
+	t_node	*current;
+	t_node	*next;
+
+	if (!head || !*head)
+		return ;
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		ft_cleanup_node(&current);
+		current = next;
+	}
+	*head = NULL;
+}
+
+void	ft_cleanup_env(t_env *env)
+{
+	if (!env)
+		return ;
+	if (env->pile)
+	{
+		ft_cleanup_stack(&env->pile->head_a);
+		ft_cleanup_stack(&env->pile->head_b);
+		free(env->pile);
+		env->pile = NULL;
+	}
+	if (env->nums)
+	{
+		free(env->nums);
+		env->nums = NULL;
+	}
 }
 
 void	ft_exit_error(char *msg)
